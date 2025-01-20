b863e961e5957984a978fae724021c02bb14db93
herify <herifyram@gmail.com>
2025-01-20 17:24:26 -0500

    test deploy github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output : 'export',
+  basePath : 'https://github.com/worstMev/finance_bot.git'
 };
 
 export default nextConfig;
